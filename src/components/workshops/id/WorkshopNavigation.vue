4aac6db295ea423388209a31e64b3e4c43ca49a9
Sergey Lebedev <sergey.lebedev@genesys.com>
2023-01-23 14:35:03 -0800

    WS Analytics (navigation events)

--- src/components/workshops/id/WorkshopNavigation.vue
+++ src/components/workshops/id/WorkshopNavigation.vue
@@ -3,11 +3,13 @@ import type { ITree } from '@/interfaces/workshop'
 import router from '@/router';
 import { useUserStore } from '@/stores/user';
 import { useWorkshopStore } from '@/stores/workshop';
+import { sendWorkshopEvent } from "@/services/analytics";
 
 const wStore = useWorkshopStore()
 const { workshopTree, workshopProgress, workshopTreeKey, urlParam, wsId } = storeToRefs(wStore)
 const { setTreeIndexByKey, treeChange, rebuildTree, loadWorkshop, setTreeIndexByPath } = wStore
 const userStore = useUserStore()
+
 const { localization } = storeToRefs(userStore)
 
 const defaultProps = {
@@ -37,6 +39,7 @@ onBeforeRouteUpdate(async (to, from) => {
   const newPath = to.path.replace(`/workshops/${wsId.value}/`, '')
   setTreeIndexByPath(newPath.startsWith('./') ? newPath.substring(2) : newPath);
   treeRef.value?.setCurrentKey(workshopTreeKey.value, true);
+  sendWorkshopEvent();
 })
 
 const setNewCurrentKey = () => {
